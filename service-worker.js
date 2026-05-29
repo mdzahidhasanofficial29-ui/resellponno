b2475a503de277a0d81e5152b0ebde7945ccd422
mdzahidhasanofficial29-ui <mdzahidhasanofficial29@gmail.com>
2026-05-29 12:17:16 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,16 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return new Response("Network error occurred");
+    })
+  );
+});
 
